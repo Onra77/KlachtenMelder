1f78a9a95abd253f5a5306b39420d15c0d42d1cd
Onra77 <onra77@hotmail.com>
2019-01-18 13:34:24 +0100

    Dummy data for MVP.

--- resources/views/overzicht.blade.php
+++ resources/views/overzicht.blade.php
@@ -10,10 +10,10 @@
 @section('content')
     <form id="klacht">
                        
-            <h1 id="h"> ZOEK KLACHT</h1>
+            <h1 id="h">Overzicht klachten</h1>
             <!-- <label  for="exampleFormControlInput1"> Postcode</label> -->
 
-            <small id="emailHelp" ><i class="far fa-edit"></i> Voer alleen Nederlandse Postcodes in!</small>
+            <!-- <small id="emailHelp" ><i class="far fa-edit"></i> Voer alleen Nederlandse Postcodes in!</small>
 
             <input type="text"  class="form-control" id="zip" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Postcode">
             </div>
@@ -21,7 +21,54 @@
             <label for="exampleFormControlTextarea1"><i class="fas fa-glasses"></i> Voorkomende klachten</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
         </div>
-        <button type="button" class="btn btn-primary my-1" onclick="lonlat()">Zoek op Postcode</button>
+        <button type="button" class="btn btn-primary my-1" onclick="lonlat()">Zoek op Postcode</button> -->
+
+        <table id="dummy">
+        <tr>
+        <th>Postcode</th>
+        <th>Over</th>
+        <th>Klacht</th>
+        </tr>
+        </tr>
+        <td>1234 AB</td>
+        <td>Stoeptegels</td>
+        <td>Ze liggen los en scheef</td>
+        </tr>
+        <tr>
+        <td>5678 CD</td>
+        <td>Vuilnis</td>
+        <td>Afval los op straat e.d.</td>
+        </tr>
+        <tr>
+        <td>9012 EF</td>
+        <td>Fietsers</td>
+        <td>Zonder licht</td>
+        </tr>
+        <tr>
+        <td>3456 GH</td>
+        <td>Buren</td>
+        <td>Elke avond wordt er geboord.</td>
+        </tr>
+        <tr>
+        <td>7891 IJ</td>
+        <td>Pinautomaten</td>
+        <td>Te weinig biljetten</td>
+        </tr>
+        <tr>
+        <td>2345 KL</td>
+        <td>Buren</td>
+        <td>Houtrook</td>
+        </tr>
+
+        </table>
+
+
+
+
+
+        <tr></tr>
+        </table>
+
 
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,24 @@ Route::get('contact', 'ContactController@create')->name('contact.create');
 Route::post('contact', 'ContactController@store')->name('contact.store');
 
 // Route::post('contact', 'ContactController@showForm')name('contact.show');
+
+// Route::get('overzicht', function () {
+//     $klachten = [
+//         new Klacht('1234AB', 'klacht', 'klacht1'),
+//         new Klacht('4567QW', 'klacht2', 'klacht2')
+//     ];
+
+//     return view('jaep', ['klachten' => $klachten]);
+// });
+
+class Klacht {
+    public $postcode;
+    public $categorie;
+    public $omschrijving;
+
+    public function __construct($postcode, $categorie, $omschrijving) {
+        $this->postcode = $postcode;
+        $this->categorie = $categorie;
+        $this->omschrijving = $omschrijving;
+    }
+}
