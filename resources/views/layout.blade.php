9d3ee2699d1a55ae5462a35f78355b78aec4216b
Onra77 <onra77@hotmail.com>
2019-01-18 14:47:20 +0100

    secure_asset

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
       
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-        <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('/css/style.css') }}" rel="stylesheet">
         
 
         <!-- Links -->
@@ -57,7 +57,7 @@
         
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/main.js') }}">
+        <script src="{{ secure_asset('js/main.js') }}">
         <script>
         @if ($errors->any())
             <div class="alert alert-danger">
