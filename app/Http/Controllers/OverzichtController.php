1e69096673b5a0e4c8fd5e9596f3947f837ea516
Onra77 <onra77@hotmail.com>
2019-01-24 23:13:43 +0100

    From views to Controller step 1.

---
+++ app/Http/Controllers/OverzichtController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OverzichtController extends Controller
+{
+    // Public function extends Controller  
+      public function index() {
+          echo "hallo";
+      }
+    
+    
+
+
+    
+         return view('overzicht' -> $overzicht]);
+}
+
+       
---
+++ app/Overzicht.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Overzicht;
+
+class Overzicht extends Model
+{
+    protected $table = 'overzichten';
+
+
+
+    $overzichtData
+
+}
